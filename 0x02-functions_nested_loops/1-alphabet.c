985f1eff75ddabc78058575c820df0b5443904ec
Fezile <fezilemnikazi@gmail.com>
2023-08-16 04:39:09 -0700

    sign

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,17 @@
 #include "main.h"
+
 /**
- * print_alphabet_x10 - print the alphabest 10 times
- * Return: void here
+ * print_alphabet - print all alphabet in lowercase
  */
-void print_alphabet_x10(void)
+
+void print_alphabet(void)
 {
-char c;
-for (c = 'a'; c <= 'z'; c++)
-{
-_putchar(c);
-}
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+
 _putchar('\n');
-}	
+}
+
+	
