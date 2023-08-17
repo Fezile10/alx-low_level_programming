ccc0137b5817b7392399f26de1a3bb6bd7835cb5
Fezile <fezilemnikazi@gmail.com>
2023-08-17 03:20:37 -0700

    Multiple main

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+void positive_or_negative(int i);
+{
+if (i > 0)
+{
+	printf("%dd is positive\n", i);
+}
+else if (i == 0)
+	printf("%dd is 0\n", i);
+else
+	printf("%dd is negative\n", i);
+}
