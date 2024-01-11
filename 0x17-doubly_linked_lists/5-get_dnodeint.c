9b5a23df6aa723d5c0c4fe16f6e6ac17de5b31df
Fezile <fezilemnikazi@gmail.com>
2024-01-11 14:16:17 -0800

    doubly linked lists

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -10,13 +10,13 @@
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	/* Traverse the list until the desired index is reached. */
-	for (; index != 0; index--)
-	{
-		if (head == NULL)
-			return (NULL);
-		head = head->next;
-	}
+/* Traverse the list until the desired index is reached. */
+for (; index != 0; index--)
+{
+if (head == NULL)
+return (NULL);
+head = head->next;
+}
 
-	return (head);
+return (head);
 }
